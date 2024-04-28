1c5cbb24c4c7c4a544e299e0fb19087618115d89
Isaque <isaquefrankli@gmail.com>
2024-04-28 19:58:52 -0300

    features component

--- src/components/home.js
+++ src/components/home.js
@@ -155,7 +155,7 @@ function Home() {
       </div>
     </div>
 
-        <div className="bg-white text-black">
+        <div className="bg-white text-black py-8">
             <Container>
             <div className='lg:flex py-12 gap-x-8'>
                   <div className='py-8 lg:py-0 px-8 lg:px-0'>
@@ -171,45 +171,62 @@ function Home() {
             </Container>      
         </div>
 
-        <div className="pitch" id="#pitch">
-            <Container style={{alignItems: 'center'}}>
-                <Row style={{marginTop: '50px', marginBottom: '30px'}} className="">
-                    <Col>
-                    <br></br>
-                    <h4 className="titulo3" style={{}} align="left">Aprenda a criar um negócio online</h4>
-                    <br></br>
-                    <p className="paragraph3">Tornar homens jovens comuns em grandes líderes e empreendedores inovadores, pensantes por conta própria.</p>
-                    <p className="paragraph3">A Quixote Club te ensina como pensar da forma que grandes sujeitos da humanidade pensavam e a sair do absoluto zero e encontrar uma área e um negócio que vai te gerar 10k de faturamento mensal.</p>
-                    <p className="paragraph3"></p>
-                    <br></br>
-                    <a href="https://pay.kiwify.com.br/FUsEqyr"><button className="buttonSale2" style={{marginTop: '10px', marginBottom: '30px'}}>Assinar Quixote Club</button></a>
-                    </Col>
-                    <Col>
-                        <img className="pics pointo" style={{alignItems: 'center'}} src="../img/cover.jpg" alt="lâmpada" />
-                    </Col>
-                </Row>
+        <div className="bg-black text-white py-8">
+            <Container className=''>
+                <div className='lg:flex py-12 gap-x-8'>
+                    <div className='my-auto items-center mx-8'>
+                        <h4 className="font-semibold text-4xl mb-4" style={{}} align="left">Aprenda a criar um negócio online</h4>
+                        <p className="mb-4">Tornar homens jovens comuns em grandes líderes e empreendedores inovadores, pensantes por conta própria.</p>
+                        <p className="mb-8">A Quixote Club te ensina como pensar da forma que grandes sujeitos da humanidade pensavam e a sair do absoluto zero e encontrar uma área e um negócio que vai te gerar 10k de faturamento mensal.</p>
+                        <a href="https://pay.kiwify.com.br/FUsEqyr"><button className="buttonSale2" style={{marginTop: '10px', marginBottom: '30px'}}>Assinar Quixote Club</button></a>
+                    </div>
+                    <div className='py-8 lg:py-0 px-8 lg:px-0'>
+                        <img className="transition ease-in-out delay-150 bg-blue-500 hover:-translate-y-1 hover:scale-110 duration-300 rounded rounded-2xl" style={{alignItems: 'center'}} src="https://images.unsplash.com/photo-1531297484001-80022131f5a1?q=80&w=2020&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="lâmpada" />
+                    </div>
+                </div>
             </Container>
         </div>
 
-        <div className="bg-white text-gray-800">
-            <Container style={{alignItems: 'center'}}>
-                <Row style={{marginTop: '50px', marginBottom: '30px'}} className="">
-                    <Col>
-                    <br></br>
-                    <h4 className="" style={{}} align="left">Aprenda a criar um negócio online</h4>
-                    <br></br>
-                    <p className="">Tornar homens jovens comuns em grandes líderes e empreendedores inovadores, pensantes por conta própria.</p>
-                    <p className="">A Quixote Club te ensina como pensar da forma que grandes sujeitos da humanidade pensavam e a sair do absoluto zero e encontrar uma área e um negócio que vai te gerar 10k de faturamento mensal.</p>
-                    <p className=""></p>
-                    <br></br>
-                    <a href="https://pay.kiwify.com.br/FUsEqyr"><button className="buttonSale2" style={{marginTop: '10px', marginBottom: '30px'}}>Assinar Quixote Club</button></a>
-                    </Col>
-                    <Col>
-                        <img className="pics pointo" style={{alignItems: 'center'}} src="../img/cover.jpg" alt="lâmpada" />
-                    </Col>
-                </Row>
-            </Container>
-        </div>
+        <div className='bg-black text-white'>
+        <Container className="py-16">
+            <div className='grid grid-cols-2 lg:grid-cols-4 gap-y-8 text-center'>
+                <div className='mx-auto my-auto justify-center items-center'>
+                    <svg class="w-24 h-24 text-gray-800 dark:text-white mx-auto my-auto justify-center mb-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+                        <path fill-rule="evenodd" d="M4 3a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h1v2a1 1 0 0 0 1.707.707L9.414 13H15a1 1 0 0 0 1-1V4a1 1 0 0 0-1-1H4Z" clip-rule="evenodd"/>
+                        <path fill-rule="evenodd" d="M8.023 17.215c.033-.03.066-.062.098-.094L10.243 15H15a3 3 0 0 0 3-3V8h2a1 1 0 0 1 1 1v8a1 1 0 0 1-1 1h-1v2a1 1 0 0 1-1.707.707L14.586 18H9a1 1 0 0 1-.977-.785Z" clip-rule="evenodd"/>
+                    </svg>
+
+                    <h2 className='text-lg lg:text-xl lg:font-semibold'>Grupo de networking</h2>
+                    <p></p>
+                </div>
+                <div className='mx-auto my-auto justify-center items-center'>
+                    <svg class="w-24 h-24 text-gray-800 dark:text-white mx-auto my-auto justify-center mb-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+                        <path fill-rule="evenodd" d="M21.7 8.037a4.26 4.26 0 0 0-.789-1.964 2.84 2.84 0 0 0-1.984-.839c-2.767-.2-6.926-.2-6.926-.2s-4.157 0-6.928.2a2.836 2.836 0 0 0-1.983.839 4.225 4.225 0 0 0-.79 1.965 30.146 30.146 0 0 0-.2 3.206v1.5a30.12 30.12 0 0 0 .2 3.206c.094.712.364 1.39.784 1.972.604.536 1.38.837 2.187.848 1.583.151 6.731.2 6.731.2s4.161 0 6.928-.2a2.844 2.844 0 0 0 1.985-.84 4.27 4.27 0 0 0 .787-1.965 30.12 30.12 0 0 0 .2-3.206v-1.516a30.672 30.672 0 0 0-.202-3.206Zm-11.692 6.554v-5.62l5.4 2.819-5.4 2.801Z" clip-rule="evenodd"/>
+                    </svg>
+
+                    <h2 className='text-lg lg:text-xl lg:font-semibold'>Aulas e tutoriais em vídeo</h2>
+                    <p></p>
+                </div>
+                <div className='mx-auto my-auto justify-center items-center'>
+                    <svg class="w-24 h-24 text-gray-800 dark:text-white mx-auto my-auto justify-center mb-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+                        <path d="M20 14h-2.722L11 20.278a5.511 5.511 0 0 1-.9.722H20a1 1 0 0 0 1-1v-5a1 1 0 0 0-1-1ZM9 3H4a1 1 0 0 0-1 1v13.5a3.5 3.5 0 1 0 7 0V4a1 1 0 0 0-1-1ZM6.5 18.5a1 1 0 1 1 0-2 1 1 0 0 1 0 2ZM19.132 7.9 15.6 4.368a1 1 0 0 0-1.414 0L12 6.55v9.9l7.132-7.132a1 1 0 0 0 0-1.418Z"/>
+                    </svg>
+
+                    <h2 className='text-lg lg:text-xl lg:font-semibold'>Ebooks, planilhas e outros.</h2>
+                    <p></p>
+                </div>
+                <div className='mx-auto my-auto justify-center items-center'>
+                    <svg class="w-24 h-24 text-gray-800 dark:text-white mx-auto my-auto justify-center mb-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 13v-2a1 1 0 0 0-1-1h-.757l-.707-1.707.535-.536a1 1 0 0 0 0-1.414l-1.414-1.414a1 1 0 0 0-1.414 0l-.536.535L14 4.757V4a1 1 0 0 0-1-1h-2a1 1 0 0 0-1 1v.757l-1.707.707-.536-.535a1 1 0 0 0-1.414 0L4.929 6.343a1 1 0 0 0 0 1.414l.536.536L4.757 10H4a1 1 0 0 0-1 1v2a1 1 0 0 0 1 1h.757l.707 1.707-.535.536a1 1 0 0 0 0 1.414l1.414 1.414a1 1 0 0 0 1.414 0l.536-.535 1.707.707V20a1 1 0 0 0 1 1h2a1 1 0 0 0 1-1v-.757l1.707-.708.536.536a1 1 0 0 0 1.414 0l1.414-1.414a1 1 0 0 0 0-1.414l-.535-.536.707-1.707H20a1 1 0 0 0 1-1Z"/>
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 15a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z"/>
+                    </svg>
+
+                    <h2 className='text-lg lg:text-xl lg:font-semibold'>Assinatura gratuita de softwares.</h2>
+                    <p></p>
+                </div>
+            </div>
+        </Container>
+    </div>
 
         <div className="services" id="#services">
             <Container style={{marginTop: '70px', marginBottom: '70px'}}>
