093a4ce67a42da35fcf832c5e5ee25362f554fec
Isaque <isaquefrankli@gmail.com>
2024-05-11 19:30:09 -0300

    double plan and pay links

--- src/components/home.js
+++ src/components/home.js
@@ -141,12 +141,12 @@ function Home() {
             </p>
             <div className="mt-10 flex items-center justify-center gap-x-6">
               <a
-                href="https://pay.kiwify.com.br/FUsEqyr"
+                href="https://pay.kiwify.com.br/9m4od8V"
                 className="rounded-md bg-blue-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-gray-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               >
                 Fazer parte
               </a>
-              <a href="#" className="text-sm font-semibold leading-6 text-white">
+              <a href="/about" className="text-sm font-semibold leading-6 text-white">
                 Saber mais <span aria-hidden="true">→</span>
               </a>
             </div>
@@ -165,7 +165,7 @@ function Home() {
                     <h2 className="text-2xl lg:text-4xl font-semibold" style={{}}>Uma assinatura para o empreendedor digital</h2>
                         <p className="" style={{marginTop: '20px', marginBottom: '0px', textAlign: 'left'}}>Do plebeu ao rei, essa é a Quixote Club, uma comunidade que vai te ensinar desde mentalidade e livros grandiosos até como fazer dinheiro do absoluto zero com a internet sendo jovem e perdido na vida.</p>
                         <br></br>
-                    <a className='rounded-sm mt-8 px-8 py-3 text-sm shadow-sm font-semibold bg-[#08A142] text-white hover:bg-gray-800 hover:scale-110' href="https://pay.kiwify.com.br/FUsEqyr">Fazer parte agora</a>
+                    <a className='rounded-sm mt-8 px-8 py-3 text-sm shadow-sm font-semibold bg-[#08A142] text-white hover:bg-gray-800 hover:scale-110' href="https://pay.kiwify.com.br/9m4od8V">Fazer parte agora</a>
                   </div>
             </div>
             </Container>      
@@ -179,7 +179,7 @@ function Home() {
                         <p className="mb-4">Tornar homens jovens comuns em grandes líderes e empreendedores inovadores, pensantes por conta própria.</p>
                         <p className="mb-8">A Quixote Club te ensina como pensar da forma que grandes sujeitos da humanidade pensavam e a sair do absoluto zero e encontrar uma área e um negócio que vai te gerar 10k de faturamento mensal.</p>
                         <h4 className='font-semibold text-xl mb-8'>Apenas R$29,90/mês, aproveite a oportunidade de mudar sua vida com a internet.</h4>
-                        <a href="https://pay.kiwify.com.br/FUsEqyr"><button className="buttonSale2" style={{marginTop: '10px', marginBottom: '30px'}}>Assinar Quixote Club</button></a>
+                        <a href="https://pay.kiwify.com.br/9m4od8V"><button className="buttonSale2" style={{marginTop: '10px', marginBottom: '30px'}}>Assinar Quixote Club</button></a>
                     </div>
                     <div className='py-8 lg:py-0 px-8 lg:px-0'>
                         <img className="transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300 rounded rounded-2xl" style={{alignItems: 'center'}} src="https://images.unsplash.com/photo-1531297484001-80022131f5a1?q=80&w=2020&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="lâmpada" />
@@ -252,11 +252,41 @@ function Home() {
                     <h2 className="text-2xl lg:text-4xl font-semibold" style={{}}>Softwares desenvolvidos especificamente para ajudar o empreendedor digital.</h2>
                         <p className="" style={{marginTop: '20px', marginBottom: '0px', textAlign: 'left'}}>Do plebeu ao rei, essa é a Quixote Club, uma comunidade que vai te ensinar desde mentalidade e livros grandiosos até como fazer dinheiro do absoluto zero com a internet sendo jovem e perdido na vida.</p>
                         <br></br>
-                        <a href="https://pay.kiwify.com.br/FUsEqyr"><button className="buttonSale2" style={{marginTop: '10px', marginBottom: '30px'}}>Assinar Quixote Club</button></a>
+                        <a href="https://pay.kiwify.com.br/9m4od8V"><button className="buttonSale2" style={{marginTop: '10px', marginBottom: '30px'}}>Assinar Quixote Club</button></a>
 
                   </div>
             </div>
             </Container>      
+        </div>
+
+        <div className='bg-white text-black py-2 lg:py-8'>
+          <Container>
+            <div className='pb-12 grid lg:grid-cols-2 justify-center my-auto mx-auto gap-8 max-w-4xl'>
+                <div className='rounded-2xl border border-success border-3 py-12 px-8'>
+                  <h2 className='text-4xl font-semibold'>Plano anual</h2>
+                  <ol className='py-4 list-decimal text-xl px-4'>
+                    <li>Grupo da comunidade</li>
+                    <li>Ebboks, planilhas e pdfs</li>
+                    <li>Softwares exclusivos</li>
+                    <li>Encontros no Zoom</li>
+                    <li>Clube do livro</li>
+                    <li>Assinatura do Horizon Pro</li>
+                  </ol>
+                  <h2 className='text-2xl font-light pb-8'>R$29,90/mês</h2>
+                  <a className='rounded-md mt-8 px-8 py-3 text-sm shadow-sm font-semibold bg-[#08A142] text-white hover:bg-gray-800 hover:scale-110' href="https://pay.kiwify.com.br/9m4od8V">Fazer parte agora</a>
+                </div>
+                <div className='rounded-2xl border-2 border-black py-12 px-8'>
+                  <h2 className='text-4xl font-semibold'>Plano mensal</h2>
+                  <ol className='py-4 list-decimal text-xl px-4'>
+                    <li>Grupo da comunidade</li>
+                    <li>Ebboks, planilhas e pdfs</li>
+                    <li>Softwares exclusivos</li>
+                  </ol>
+                  <h2 className='text-2xl font-light pb-8'>R$49,90/mês</h2>
+                  <a className='rounded-md mt-8 px-8 py-3 text-sm shadow-sm font-semibold bg-[#08A142] text-white hover:bg-gray-800 hover:scale-110' href="https://pay.kiwify.com.br/lUmtGig">Fazer parte agora</a>
+                </div>
+            </div>
+          </Container>
         </div>
 
 
@@ -268,15 +298,15 @@ function Home() {
                   <h2 className='text-white font-semibold text-2xl mb-8'>Grupo da comunidade</h2>
                   <div className='lg:flex mx-auto justify-center'>
                     <div className='w-64 h-64 lg:mb-0 mb-8 mx-auto'>
-                      <img className="pics rounded-2xl p-8 justify-center mx-auto w-64 h-64" src="../img/1.png" />
+                      <img className="pics rounded-2xl p-8 justify-center mx-auto w-64 h-64" src="https://image.civitai.com/xG1nkqKTMzGDvpLrqFT7WA/1c2043d1-a08c-44f6-99df-786685985576/original=true/563B72B201184582807C8A787C283D15941E09AE735A48C6B794DB23C49E4C96.jpeg" />
                       <p className="text-white text-center px-8">Networking com outros empreendedores.</p>
                     </div>
                     <div className='w-64 h-64 lg:mb-0 mb-8 mx-auto'>
-                      <img className="pics rounded-2xl p-8 justify-center mx-auto w-64 h-64" src="../img/1.png" />
+                      <img className="pics rounded-2xl p-8 justify-center mx-auto w-64 h-64" src="https://image.lexica.art/full_webp/7b2b6dfb-4f20-4df6-b09d-8e0d3dc43698" />
                       <p className="text-white text-center px-8">Possíveis sócios e clientes.</p>
                     </div>
                     <div className='w-64 h-64 lg:mb-0 mb-8 mx-auto'>
-                      <img className="pics rounded rounded-2xl p-8 justify-center mx-auto w-64 h-64" src="../img/1.png" />
+                      <img className="pics rounded rounded-2xl p-8 justify-center mx-auto w-64 h-64" src="https://image.lexica.art/full_webp/4f3b2842-5eea-43d9-adbc-c867946f3265" />
                       <p className="text-white text-center px-8">Acesso à experiência de outros empreendedores.</p>
                     </div>
                   </div>
@@ -339,7 +369,7 @@ function Home() {
                 <div className='mt-16 lg:mt-48 px-8'>
                   <h1 className='text-white text-xl lg:text-4xl font-light'>Tudo isso por apenas R$29,90 por mês.</h1>
                   <br></br>
-                  <a href="https://pay.kiwify.com.br/FUsEqyr"><button className="buttonSale2">Assinar Quixote Club</button></a>
+                  <a href="https://pay.kiwify.com.br/9m4od8V"><button className="buttonSale2">Assinar Quixote Club</button></a>
                 </div>
             </Container>
         </div>
@@ -354,7 +384,7 @@ function Home() {
                     <h2 className="text-2xl lg:text-4xl font-semibold mb-4">Aprenda a criar valor e ganhar dinheiro na internet</h2>
                         <p className="mb-4">Sem enrolação, sem falsas promessas, na Quixote Club nos ensinamos o que não ensinam em nenhum outro lugar: a realidade como ela é. Desenvolva habilidades e se torne um jovem empreendedor, faça parte do grupo de networking e conheça sócios.</p>
                         <br></br>
-                    <a className='rounded-sm mt-8 px-8 py-3 text-sm shadow-sm font-semibold bg-[#08A142] text-white hover:bg-gray-800 hover:scale-110' href="https://pay.kiwify.com.br/FUsEqyr">Quixote Club</a>
+                    <a className='rounded-sm mt-8 px-8 py-3 text-sm shadow-sm font-semibold bg-[#08A142] text-white hover:bg-gray-800 hover:scale-110' href="https://pay.kiwify.com.br/9m4od8V">Quixote Club</a>
                   </div>
             </div>
             </Container>      
